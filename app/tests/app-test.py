1ef30ad6878e91ec5938a6bdb8e375975c737566
IndranilRay <36516665+IndranilRay@users.noreply.github.com>
2020-11-15 21:27:13 +0530

    Add zeros to test function

--- app/tests/app-test.py
+++ app/tests/app-test.py
@@ -10,6 +10,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(add_two_number(-1, -1), -2)
         self.assertEqual(add_two_number(1.0, 1), 4)
         self.assertEqual(add_two_number(1.1, 1.1), 2.2)
+        self.assertEqual(add_two_number(0, 0), 0)
 
 
 if __name__ == '__main__':
